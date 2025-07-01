96ca458654ff25d0e6ca5755fb42e3cac68e86bc
Tanouu <ethanpachjeco.t@gmail.com>
2025-07-01 12:38:23 +0200

    Test docker avec le Readme

--- front/src/app/services/jokes.service.ts
+++ front/src/app/services/jokes.service.ts
@@ -2,14 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Joke } from '../model/joke.model';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JokesService {
 
-  private pathService = `${environment.apiUrl}/joke`;
+  private pathService = 'api/joke';
 
   private subject: BehaviorSubject<Joke | null> = new BehaviorSubject<Joke | null>(null);
 
@@ -18,8 +17,7 @@ export class JokesService {
   }
 
   public getRandomJoke(): void {
-    this.httpClient.get<Joke>(this.pathService)
-      .subscribe((joke: Joke) => this.subject.next(joke));
+    this.httpClient.get<Joke>(this.pathService).subscribe((joke: Joke) => this.subject.next(joke));
   }
 
   public joke$(): Observable<Joke | null > {
